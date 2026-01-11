96147c267c831732142913c3729ae686a7221542
sripavan24 <sripavanvannam@gmail.com>
2026-01-11 15:58:10 +0530

    Added EDA and model training code

--- sor/exception.py
+++ sor/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from logger import logging
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
@@ -21,7 +21,7 @@ class CustomException(Exception):
     
 """if __name__=="__main__":
     try:
-        a=1/0
+        a=1/1
     except Exception as e:
         logging.info("Logging has started")
         raise CustomException(e,sys)"""
